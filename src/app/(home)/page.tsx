6356404d3ad8f80574a51718378e8f0093506abf
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-10-16 21:30:03 -0300

    add create and login

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { url } from '@/src/utils/url';
 import { IProducts } from '@/src/types/IProducts';
 import HomeBanner from './sections/banner';
 import HomeItems from './sections/items';
+import Link from 'next/link';
 
 async function getAllProdutcsData(): Promise<IProducts> {
   try {
@@ -19,6 +20,9 @@ export default async function Home() {
 
   return (
     <main className='font-open_Sans relative bg-red-500 py-8'>
+      <Link href='/login'>Login</Link>
+      <br />
+      <Link href='/cadastro'>Cadastrar</Link>
       <HomeBanner />
       <HomeItems products={dataProducts?.products} />
     </main>
--- src/app/cadastro/sections/form.tsx
+++ src/app/cadastro/sections/form.tsx
@@ -7,6 +7,10 @@ import { api } from '@/src/service/api';
 import { useRouter } from 'next/navigation';
 
 const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, 'Campo obrigatório')
+    .refine((a) => a.trim() !== '', 'Campo obrigatório'),
   password: z.string(),
   email: z
     .string()
@@ -16,7 +20,7 @@ const formSchema = z.object({
 
 type FormValues = z.infer<typeof formSchema>;
 
-export default function LoginForm() {
+export default function RegisterForm() {
   const router = useRouter();
   const { register, handleSubmit } = useForm<FormValues>({
     resolver: zodResolver(formSchema),
@@ -26,7 +30,7 @@ export default function LoginForm() {
 
   const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
     try {
-      await api.post('/loginUser', data);
+      await api.post('/createUser', data);
       router.push('/');
     } catch (error) {
       console.log(error);
@@ -37,9 +41,10 @@ export default function LoginForm() {
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <h3>LOGIN</h3>
-          <input {...register('email')} placeholder='Email'></input>
+          <h3>CADASTRAR</h3>
+          <input {...register('name')} placeholder='Username'></input>
           <input {...register('password')} placeholder='Password'></input>
+          <input {...register('email')} placeholder='Email'></input>
         </div>
         <button type='submit'>ENTRAR</button>
       </form>
