fea3643f3e172258594f3f0857671b61a65d821a
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-10-27 16:54:08 -0300

    fix finish checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -15,15 +15,15 @@ const formSchema = z.object({
     name: z.string(),
     number: z.string(),
     cvc: z.string(),
-    expiryMonth: z.string(),
-    expiryYear: z.string(),
+    expiryMonth: z.number(),
+    expiryYear: z.number(),
   }),
 });
 
 type FormValues = z.infer<typeof formSchema>;
 
 export default function Checkout() {
-  const { register, handleSubmit, watch, formState } = useForm<FormValues>({
+  const { register, handleSubmit, watch } = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     // mode: 'onChange',
     // reValidateMode: 'onChange',
@@ -38,14 +38,13 @@ export default function Checkout() {
   const { cartItems } = useCart();
 
   // handleInputFocus = (e) => {
-  //   this.setState({ focus: e.target.name });
+  //  setState({ focus: e.target.name });
   // }
 
   const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
-    // try {
     await api.post(`/buyProduct/${parseCookies().blume_user_id}`, {
       productId: cartItems.map((product) => {
-        product.id;
+        return product.id;
       }),
       installments: data.installments,
       cardInfos: {
@@ -56,9 +55,6 @@ export default function Checkout() {
         cvv: data.creditCard.cvc,
       },
     });
-    // } catch (error) {
-    // console.log('Precisa estar logado');
-    // }
   };
 
   return (
@@ -91,20 +87,22 @@ export default function Checkout() {
         <input
           placeholder='Validade mes'
           onFocus={() => setFocus('expiry')}
-          {...register('creditCard.expiryMonth')}
+          {...register('creditCard.expiryMonth', {
+            setValueAs: (v: string) => Number(v),
+          })}
         ></input>
         <input
           placeholder='Validade ano'
           onFocus={() => setFocus('expiry')}
-          {...register('creditCard.expiryYear')}
+          {...register('creditCard.expiryYear', {
+            setValueAs: (v: string) => Number(v),
+          })}
         ></input>
         <CreditCard
           name={name || ''}
           focused={focus}
           expiry={
-            expiryYear && expiryMonth
-              ? `${expiryMonth.padStart(2, '0')}/${expiryYear}`
-              : '****'
+            expiryYear && expiryMonth ? `${expiryMonth}/${expiryYear}` : '****'
           }
           locale={{ valid: 'VALIDADE' }}
           number={number || '**** **** **** ****'}
