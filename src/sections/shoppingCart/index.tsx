14102d3e9dc16acd8598f10267373d33143fdb48
ViniciusBenfica <vinicius.benfica.ruy@gmail.com>
2023-11-17 12:37:59 -0300

    add shopping cart and new lib

--- src/sections/shoppingCart/index.tsx
+++ src/sections/shoppingCart/index.tsx
@@ -1,17 +1,49 @@
 'use client';
 import { useCart } from '@/src/providers/shoppingCartProvider';
 import Link from 'next/link';
+import Image from 'next/image';
+import CartImage from '/public/blume.png';
+import { useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export default function ShoppingCart() {
+  const [isOpen, setIsOpen] = useState(true);
   const { cartItems } = useCart();
+
   return (
-    <div>
-      <p>Carrinho</p>
-      <p>produtos:</p>
-      {cartItems.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
-      <Link href={`/checkout`}>FINALIZAR COMPRA</Link>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          <Image src={CartImage} width={30} height={30} alt='cart image' />
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className='data-[state=open]:animate-overlayShow fixed inset-0 bg-black/50' />
+        <Dialog.Content className='absolute right-0 top-0 h-screen w-[500px] bg-red-700 '>
+          <div className='flex justify-between'>
+            <Dialog.Close asChild>
+              <button>CLOSE</button>
+            </Dialog.Close>
+            <Dialog.Description className='text-mauve11 mb-5 mt-[10px] text-[15px] leading-normal'>
+              Make changes to your profile here. Click save when done.
+            </Dialog.Description>
+          </div>
+          <div className='mb-[15px] flex items-center gap-5'>ITEM 1</div>
+          <div className='mb-[15px] flex items-center gap-5'>ITEM 1</div>
+          <div className='mb-[15px] flex items-center gap-5'>ITEM 1</div>
+          <div className='mb-[15px] flex items-center gap-5'>ITEM 1</div>
+
+          <div>
+            <div className='flex'>
+              <p>Subtotal</p>
+              <p>50</p>
+            </div>
+            <Dialog.Close asChild>
+              <Link href={`/checkout`}>FINALIZAR COMPRA</Link>
+            </Dialog.Close>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
