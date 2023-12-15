c82b7c10f57348a01635891cf501a95feede5e8b
Vinicius <vinicius.benfica.ruy@gmail.com>
2023-12-15 19:52:07 -0300

    add env and fix

--- src/app/(home)/sections/home.items.tsx
+++ src/app/(home)/sections/home.items.tsx
@@ -10,18 +10,18 @@ export default function HomeItems({ categories }: IHomeItems) {
   return (
     <div className='m-auto pt-[70px] sm:w-sm md:w-md lg:w-lg xl:w-xl xxl:w-xxl xxxl:w-xxxl'>
       {categories.map((category) => (
-        <div key={category.id}>
+        <div key={category?.id}>
           <div className='flex justify-between'>
             <div className='mb-5 text-xl font-bold text-black'>
-              {category.name}
+              {category?.name}
             </div>
             <Link href='/produto' className='text-sm text-black/50'>
               VER TODOS
             </Link>
           </div>
           <div className='grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 xxl:grid-cols-4'>
-            {category.products.slice(0, 4).map((product) => (
-              <ProductCard key={product.id} product={product} />
+            {category?.products?.slice(0, 4).map((product) => (
+              <ProductCard key={product?.id} product={product} />
             ))}
           </div>
         </div>
--- src/sections/layout/header.tsx
+++ src/sections/layout/header.tsx
@@ -4,7 +4,7 @@ import BlumeIcon from '/public/img/blume.png';
 
 export default function Header() {
   return (
-    <header className='fixed z-10 flex h-[70px] w-full items-center justify-between bg-blue-900'>
+    <header className='fixed z-10 flex h-[70px] w-full items-center justify-between bg-black/30'>
       <div className='m-auto flex items-center justify-between sm:w-sm md:w-md lg:w-lg xl:w-xl xxl:w-xxl xxxl:w-xxxl'>
         <div className='flex items-center gap-3'>
           <div className='text-xl font-bold text-white'>BLUME ROLEPLAY</div>
@@ -20,9 +20,9 @@ export default function Header() {
           className='h-[32px] w-2/4 rounded'
           placeholder='O que está procurando?'
         />
-        <div className='flex gap-5'>
+        <div className='flex items-center gap-5'>
           <div className='text-base font-medium text-white'>
-            <div>Login</div>
+            <div className='bg-'>Login</div>
             <div>Cadastrar</div>
           </div>
           <ShoppingCart />
