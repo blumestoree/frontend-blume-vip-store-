bd24fd57ff48a7f18cc832f73512920c8e08d0e8
Vinicius <vinicius.benfica.ruy@gmail.com>
2024-01-01 22:25:48 -0300

    add link in logo

--- src/sections/layout/header.tsx
+++ src/sections/layout/header.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import ShoppingCart from '@/src/components/shoppingCart';
 import BlumeIcon from '/public/img/blume.png';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className='fixed z-10 flex h-[70px] w-full items-center justify-between bg-[#101010]'>
       <div className='grid-style flex items-center justify-between'>
-        <div className='flex items-center gap-3'>
+        <Link className='flex items-center gap-3' href={'/'}>
           <div className='text-xl font-bold text-white'>BLUME ROLEPLAY</div>
           <Image
             className='m-auto'
@@ -15,7 +16,7 @@ export default function Header() {
             height={30}
             alt='blume icon'
           />
-        </div>
+        </Link>
         <input
           className='h-[32px] w-2/4 rounded'
           placeholder='O que está procurando?'
