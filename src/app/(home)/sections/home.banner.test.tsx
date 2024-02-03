8ffc8b5e3df5e09b26c61c0cf6b13ca40e83d2f3
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-02-03 20:11:59 -0300

    create teste for banner

---
+++ src/app/(home)/sections/home.banner.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import HomeBanner from './home.banner';
+
+describe('HomeBanner', () => {
+  it('should render the banner with correct attributes', () => {
+    render(<HomeBanner />);
+
+    const bannerImage = screen.getByAltText('banner') as HTMLImageElement;
+
+    expect(bannerImage).toBeTruthy();
+
+    expect(bannerImage.width).toBe(1920);
+    expect(bannerImage.height).toBe(400);
+  });
+});
