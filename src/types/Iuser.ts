c8e5e7d105cfb757c222293e8defa4119c550a2b
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-08-11 20:11:40 -0300

    🏷️ add user types

--- src/types/Iuser.ts
+++ src/types/Iuser.ts
@@ -1,8 +1,40 @@
 export interface Iuser {
-	name: string;
-	token: string;
 	id: string;
+	name: string;
+	email: string;
+	token: string;
+	userOnServer: {
+		id: string;
+		nickname: string;
+		gameUserId: string;
+		server: {
+			id: string;
+			name: string;
+			image: string;
+		};
+	}[];
 	refreshToken: {
 		id: string;
+		expiresIn: number;
+	};
+}
+export interface IuserApi {
+	id: string;
+	name: string;
+	email: string;
+	token: string;
+	userOnServer: {
+		id: string;
+		nickname: string;
+		gameUserId: string;
+		server: {
+			id: string;
+			name: string;
+			image: string;
+		};
+	}[];
+	refreshToken: {
+		id: string;
+		expiresIn: number;
 	};
 }
