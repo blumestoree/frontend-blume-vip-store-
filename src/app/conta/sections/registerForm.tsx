a9c61287ac41d172fc2d9ae2813660b444a738a9
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-08-11 20:11:23 -0300

    🐛 start fix create user

--- src/app/conta/sections/registerForm.tsx
+++ src/app/conta/sections/registerForm.tsx
@@ -17,6 +17,7 @@ const formSchema = z.object({
 	password: z.string(),
 	email: z.string().email("Email inválido"),
 	gameUserId: z.string(),
+	nickname: z.string(),
 });
 
 type FormValues = z.infer<typeof formSchema>;
@@ -29,24 +30,30 @@ export default function RegisterForm() {
 		// reValidateMode: 'onChange',
 	});
 
-	const createUser = async (httpClient: httpClient<Iuser>, data: any) => {
-	  return await httpClient.request({
+	const createUser = async (httpClient: httpClient<Iuser>, data: FormValues) => {
+		return await httpClient.request({
 			url: "/createUser",
 			method: "post",
-			body: data,
+			body: {
+				name: data.name,
+				email: data.email,
+				password: data.password,
+				gameUserId: data.gameUserId,
+				serverId: "1",
+				nickname: data.nickname,
+			},
 		});
 	};
 
 	const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
 		try {
 			const dataUser = await createUser(axiosHttpAdapter, data);
-
 			const userData = createUserMapper(dataUser.body);
 			setCookie(null, "blume_user_data", JSON.stringify(userData), {
-				maxAge: 30,
+				maxAge: 300000,
 			});
 
-			// router.push("/");
+			router.push("/");
 		} catch (error) {
 			console.log(error);
 		}
@@ -55,26 +62,26 @@ export default function RegisterForm() {
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className="flex w-[400px] flex-col items-center gap-4">
-				<h3 className="text-2xl font-bold">CRIAR CONTA</h3>
+				<h3 className="font-bold text-2xl">CRIAR CONTA</h3>
 				<div className="flex w-full flex-col">
 					<label className="text-xs">Nome</label>
-					<input className="h-8 border" {...register("name")} placeholder="Name"></input>
+					<input className="h-8 border" {...register("name")} placeholder="Name" />
 				</div>
 				<div className="flex w-full flex-col">
 					<label className="text-xs">Email</label>
-					<input className="h-8 border" {...register("email")} placeholder="Email"></input>
+					<input className="h-8 border" {...register("email")} placeholder="Email" />
 				</div>
 				<div className="flex w-full flex-col">
 					<label className="text-xs">Password</label>
-					<input className="h-8 border" {...register("password")} placeholder="Password"></input>
+					<input className="h-8 border" {...register("password")} placeholder="Password" />
 				</div>
 				<div className="flex w-full flex-col">
 					<label className="text-xs">ID</label>
-					<input
-						className="h-8 border"
-						{...register("gameUserId")}
-						placeholder="ID no jogo"
-					></input>
+					<input className="h-8 border" {...register("gameUserId")} placeholder="ID no jogo" />
+				</div>
+				<div className="flex w-full flex-col">
+					<label className="text-xs">Nome no jogo</label>
+					<input className="h-8 border" {...register("nickname")} placeholder="Nome no jogo" />
 				</div>
 				<button className="h-8 w-full bg-blue-500 text-white" type="submit">
 					CRIAR CONTA
