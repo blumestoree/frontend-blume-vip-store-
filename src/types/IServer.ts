c6b4cf7ddcfebfb01d05d675073cefb715685eeb
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-08-28 21:29:03 -0300

    ✨ add serverInterface

---
+++ src/types/IServer.ts
@@ -0,0 +1,20 @@
+export interface IServer {
+	id: string;
+  name: string;
+  slug: string;
+  image: string;
+  banner: string[];
+  serverOwnerId: string;
+  products: string[];
+  categories: string[];
+}
+export interface IServerApi {
+	id: string;
+  name: string;
+  slug: string;
+  image: string;
+  banner: string[];
+  serverOwnerId: string;
+  products: string[];
+  categories: string[];
+}
